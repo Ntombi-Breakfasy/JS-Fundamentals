80be14faa714978db9144f0e10d0d3357c8ce25c
Ntombi Breakfasy <ntombibreakfasy2@gmail.com>
2025-07-13 22:25:17 +0200

    comminting more files to repo

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,17 @@
+
+const messages=[
+    "C is fun",
+    "Python is cool",
+    "Javascript is amazing",
+
+];
+
+let output="";
+for (let i=0; i<messages.length; i++)
+{
+    output += messages [i] + (i<messages.length -1? "\n":"");
+
+}
+console.log(output);
+
+
