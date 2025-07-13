b710bc1967277ba4dd317e38d4e780d6107bb7c5
Ntombi Breakfasy <ntombibreakfasy2@gmail.com>
2025-07-13 21:13:01 +0200

    initial commit

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+const args= process.argv.slice(2);
+if (args.length==0){
+    console.log("No arguments");
+}
+else if (args==1){
+    console.log("Arugment found");
+}
+else
+{
+    console.log("Arguments founs")
+    
+}
---
+++ intro.js
@@ -0,0 +1,3 @@
+const userName = "Amina";
+console.log("Welcome, " + userName);
+
